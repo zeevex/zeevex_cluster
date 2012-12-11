58b0d045d95b648eda831efaf6ee3324d142cd4f
Robert Sanders <robert@zeevex.com>
2012-12-11 18:24:19 -0500

    DRY up some code

--- lib/zeevex_cluster/unclustered.rb
+++ lib/zeevex_cluster/unclustered.rb
@@ -1,51 +1,10 @@
+require 'zeevex_cluster/static'
+
 module ZeevexCluster
-  class Unclustered < Base
-    ##
-    ## joining is a no-op for ol' singleton here
-    ##
-    def join
-      true
+  class Unclustered < Static
+    def initialize(options = {})
+      options[:master_nodename] = :self
+      super
     end
-
-    ##
-    ## leaving, too
-    ##
-    def leave
-      true
-    end
-
-    ##
-    ## We're unclustered, so we're always the master
-    ##
-    def master?
-      true
-    end
-
-    ##
-    ## Make this node the master, returning true if successful. No-op for now.
-    ##
-    def make_master!
-      true
-    end
-
-    ##
-    ## Resign from mastership; returns false if this is the only node.
-    ##
-    ## No-op for now.
-    ##
-    def resign!
-      raise NotMaster unless master?
-
-      # master is currently fixed, so we can't resign
-      raise ClusterPolicyViolation, "Current master cannot resign in this implementation."
-    end
-
-    ##
-    ## Return name of master node
-    ##
-    def master
-      nodename
-    end
-
   end
 end
