d3f607bb53cff1eabc0f17edadd55064f8ff0fd9
Robert Sanders <robert@zeevex.com>
2012-12-12 06:45:05 -0500

    added coordinator exception class

--- lib/zeevex_cluster/coordinator.rb
+++ lib/zeevex_cluster/coordinator.rb
@@ -1,6 +1,22 @@
 module ZeevexCluster
   module Coordinator
+    # flow control exceptions used in these classes
     class DontChange < StandardError; end
+
+    # errors throw by these classes
+    class CoordinatorError < StandardError
+      attr_accessor :chained
+      def initialize(message, original = nil)
+        @chained = original
+        super(message)
+      end
+      def to_s
+        @chained ? super + "; chained = #{@chained.inspect}" : super
+      end
+    end
+
+    class ConnectionError < CoordinatorError; end
+    class ConsistencyError < CoordinatorError; end
   end
 end
 
